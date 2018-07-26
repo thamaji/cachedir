27769cec3f5991168830b74133e855369c123a2a
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2018-07-27 00:26:50 +0900

    よいしょー

---
+++ cachedir_darwin.go
@@ -0,0 +1,16 @@
+package cachedir
+
+import (
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+)
+
+func Dir() (string, error) {
+	dir, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(dir, "Library", "Caches"), nil
+}
---
+++ cachedir_windows.go
@@ -0,0 +1,10 @@
+package cachedir
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func Dir() (string, error) {
+	return filepath.Join(filepath.FromSlash(os.Getenv("LOCALAPPDATA")), "cache"), nil
+}
